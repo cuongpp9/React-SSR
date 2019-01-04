77e058c38587d16a8d19f1c22807a68c9a52c7a4
cuongpp9 <cuongpp9@gmail.com>
2019-01-04 10:14:36 +0700

    create app

---
+++ README.md
@@ -0,0 +1,19 @@
+### Run the app from a dev server
+```sh
+npm start
+```
+
+### Build the app
+```sh
+npm run build
+```
+
+### Host the app from a server (app must have been built)
+```sh
+npm run serve
+```
+
+### Switch versions (ex. v1.0.0)
+```sh
+git checkout v1.0.0
+```
---
+++ webpack.config.prod.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+
+module.exports = {
+	entry: path.resolve(__dirname, 'src'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].[chunkhash].js',
+		publicPath: '/'
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify('production'),
+				WEBPACK: true
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin({
+			compressor: {
+				warnings: false
+			}
+		}),
+		new CopyWebpackPlugin([
+			{
+				from: path.resolve(__dirname, 'src', 'assets'),
+				to: path.resolve(__dirname, 'dist', 'assets')
+			}
+		]),
+		new WebpackMd5Hash(),
+		new ExtractTextPlugin('[name].[contenthash].css')
+	],
+	module: {
+		loaders: [
+			{ 
+				test: /\.js$/,
+				use: {
+					loader: 'babel-loader'
+				},
+				include: path.resolve(__dirname, 'src')
+			},
+			{
+				test: /\.scss/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [
+						'css-loader',
+						'sass-loader',
+						{
+							loader: 'postcss-loader',
+							options: {
+								plugins: function() {
+	                return [
+	                  require('autoprefixer')
+	                ];
+	              }
+							}
+						}
+					],
+				}),
+				include: path.resolve(__dirname, 'src')
+			}
+		]
+	}
+};
