3d56efddda61c7351f0ef69496fc055ce7820e70
cuongpp9 <cuongpp9@gmail.com>
2019-01-11 17:48:57 +0700

    editor cofig rule

--- src/Layouts/home/index.js
+++ src/Layouts/home/index.js
@@ -3,21 +3,20 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { toggleTodo } from '../../redux/actions/todos';
 import Header from '../header';
-if(process.env.WEBPACK) require('./index.scss');
+if (process.env.WEBPACK) require('./index.scss');
 
 const Home = ({ dispatch, todos }) => (
 	<div className='home'>
 		<Header title='Home' />
-		
 		<div>This is home</div>
 		<br />
 		{todos.map((todo) => (
-			<div key={ todo.id }>
-				<span style={ (todo.checked) ? { textDecoration: 'line-through' } : {} }>{ todo.text } </span>
+			<div key={todo.id}>
+				<span style={(todo.checked) ? { textDecoration: 'line-through' } : {}}>{todo.text} </span>
 				<button onClick={() => dispatch(toggleTodo(todo.id))}>Toggle</button>
 			</div>
 		))}
-		<br/>
+		<br />
 		<Link to='/page'>
 			<button>Go to page</button>
 		</Link>
